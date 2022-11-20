f93a4b104e1892b81d19b0cb09ed53dfc627358c
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2022-11-20 17:55:13 +0200

    Add a simple command line functionality for listing images

--- src/list.py
+++ src/list.py
@@ -1,4 +1,7 @@
-import tomli
+import tomli, sys
+
+def read_file(path):
+    return open(path, 'rb')
 
 def parse_to_gallery(file_contents):
     return tomli.loads(file_contents)
@@ -12,6 +15,12 @@ def format(gallery):
     return rows
 
 if __name__ == "__main__":
-    images = open("../samples/images.toml", "rb")
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        print("Give a .toml gallery filename")
+        exit(0)
+    images = read_file(filename)
     parsed = tomli.load(images)
-    print(parsed)
+    formatted = format(parsed)
+    print("\n".join(formatted))
