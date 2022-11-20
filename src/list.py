5ea6d118127c4b7ed8196764c204113be021c9f7
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2022-11-20 17:16:35 +0200

    Add simple formatting functionality

--- src/list.py
+++ src/list.py
@@ -3,6 +3,12 @@ import tomli
 def parse_to_gallery(file_contents):
     return tomli.loads(file_contents)
 
+def format(gallery):
+    value = ""
+    for image in gallery.values():
+        value += image["title"] + "\n" + image["description"]
+    return value
+
 if __name__ == "__main__":
     images = open("../samples/images.toml", "rb")
     parsed = tomli.load(images)
--- src/list_test.py
+++ src/list_test.py
@@ -1,6 +1,6 @@
-import list
+import list, pytest
 
-testdata = """
+singleimage = """
 [image-1]
 hash = 123456
 title = "My first image"
@@ -8,7 +8,28 @@ description = "Image description"
 tags = ['foo', 'bar', 'baz']
 """
 
-def test_parse_to_gallery():
-    gallery = list.parse_to_gallery(testdata)
-    assert "image-1" in gallery
-    assert gallery['image-1']['hash'] == 123456
+many_images = """
+[image-1]
+hash = 123456
+title = "My first image"
+description = "Image description"
+tags = ['foo', 'bar', 'baz']
+
+[image-2]
+hash = 7890123
+title = "My second image"
+description = "Image description as well"
+tags = ['foo']
+"""
+
+@pytest.fixture
+def simple_gallery():
+    return list.parse_to_gallery(singleimage)
+
+def test_parse_to_gallery(simple_gallery):
+    assert "image-1" in simple_gallery
+    assert simple_gallery['image-1']['hash'] == 123456
+
+def test_list_single_image(simple_gallery):
+    formatted = list.format(simple_gallery)
+    assert formatted == "My first image\nImage description"
