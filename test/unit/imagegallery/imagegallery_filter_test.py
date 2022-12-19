c443e3aef0f9159c0cc2ccbc56f7571e2dafe10c
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2022-12-19 19:41:58 +0200

    Move filter test under imagegallery

--- test/unit/imagegallery/imagegallery_filter_test.py
+++ test/unit/imagegallery/imagegallery_filter_test.py
@@ -1,32 +1,32 @@
 import pytest
-from gallerycmd.list import list
+from Imagegallery.gallery_toml import filter_by_tag
 from ..data.example_gallery import example_gallery
 
 def test_filter_raises_when_no_arguments():
     with pytest.raises(TypeError):
-        list.filter_by_tag()
+        filter_by_tag()
 
 def test_filter_raises_when_one_argument():
     with pytest.raises(TypeError):
-        list.filter_by_tag({})
+        filter_by_tag({})
 
 def test_empty_tag_returns_all():
-    filtered = list.filter_by_tag(example_gallery, "")
+    filtered = filter_by_tag(example_gallery, "")
     assert filtered == example_gallery
 
 def test_tag_None_returns_all():
-    filtered = list.filter_by_tag(example_gallery, None)
+    filtered = filter_by_tag(example_gallery, None)
     assert filtered == example_gallery
 
 def test_unmathed_tag_returns_empty():
-    filtered = list.filter_by_tag(example_gallery, "nonexisting_tag")
+    filtered = filter_by_tag(example_gallery, "nonexisting_tag")
     assert filtered == {}
 
 def test_matching_tag_returns_nonempty_result():
-    filtered = list.filter_by_tag(example_gallery, "bar")
+    filtered = filter_by_tag(example_gallery, "bar")
     assert len(filtered) > 0
 
 def test_matching_tag_returns_all_hits():
-    filtered = list.filter_by_tag(example_gallery, "baz")
+    filtered = filter_by_tag(example_gallery, "baz")
     assert len(filtered) == 2
     assert filtered["path/to/image3.jpg"]["title"] == "My Third image"
