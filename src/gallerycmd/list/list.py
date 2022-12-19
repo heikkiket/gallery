1855a6591b88985b100f7701aef131b7e9406d4a
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2022-12-19 21:38:00 +0200

    Show missing files in list command

--- src/gallerycmd/list/list.py
+++ src/gallerycmd/list/list.py
@@ -4,25 +4,36 @@ from Imagegallery import Imagegallery
 from gallerycmd.parser import subparsers
 from Imagegallery.gallery_toml import filter_by_tag
 
-def format(gallery):
-    """Formats image gallery for printing out into console"""
+def format(gallery, gallery_toml=None):
+    """Formats image gallery for printing out into console
+
+    Arguments:
+    gallery - an Imagegallery object
+    gallery_toml - an alternative gallery_toml dictionary
+    """
+    toml = gallery_toml if gallery_toml else gallery.gallery_toml
     rows = []
-    for path, image in gallery.items():
+    for path, image in toml.items():
         rows.append("[" + path + "]:")
         rows.append(image["title"])
         rows.append(image["description"])
         rows.append("")
+        if "missing" in gallery.metadata[path]:
+            rows.append(" *FILE MISSING* ")
+            rows.append("")
     return rows
 
 def main(args):
     try:
         gallery = Imagegallery()
         gallery.load()
+        gallery.flag_missing()
     except FileNotFoundError:
         print("No gallery.toml file found in this directory.")
         exit(0)
 
-    formatted = format(filter_by_tag(gallery.gallery_toml, args.tag))
+    formatted = format(gallery,
+                       gallery_toml=filter_by_tag(gallery.gallery_toml, args.tag))
     print("\n".join(formatted))
 
 
--- test/unit/list/list_test.py
+++ test/unit/list/list_test.py
@@ -1,6 +1,7 @@
 import pytest
 import tomli
 
+from Imagegallery import Imagegallery
 from gallerycmd.list import list
 
 singleimage = """
@@ -27,10 +28,17 @@ tags = ['foo']
 
 @pytest.fixture
 def simple_gallery():
-    return tomli.loads(singleimage)
+    gallery = Imagegallery()
+    gallery.gallery_toml = tomli.loads(singleimage)
+    gallery._init_metadata()
+    return gallery
+
 @pytest.fixture
 def complex_gallery():
-    return tomli.loads(many_images)
+    gallery = Imagegallery()
+    gallery.gallery_toml = tomli.loads(many_images)
+    gallery._init_metadata()
+    return gallery
 
 def test_list_single_image(simple_gallery):
     formatted = list.format(simple_gallery)
@@ -40,14 +48,21 @@ def test_list_single_image(simple_gallery):
         "Image description",
         ""]
 
-def test_list_many_images(complex_gallery):
-    formatted = list.format(complex_gallery)
+def test_list_many_images(simple_gallery, complex_gallery):
+    formatted = list.format(complex_gallery, simple_gallery.gallery_toml)
+    assert formatted == [
+        "[path/to/image1.jpg]:",
+        "My first image",
+        "Image description",
+        ""]
+
+def test_missing_file(simple_gallery):
+    simple_gallery.metadata["path/to/image1.jpg"]["missing"] = True
+    formatted = list.format(simple_gallery)
     assert formatted == [
         "[path/to/image1.jpg]:",
         "My first image",
         "Image description",
         "",
-        "[path/to/image2.jpg]:",
-        "My second image",
-        "Image description as well",
+        " *FILE MISSING* ",
         ""]
