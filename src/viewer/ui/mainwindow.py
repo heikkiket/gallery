41018d4e1b21e9349c3d08287e9d733f8de0cb2e
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-01-16 23:21:17 +0200

    Add a simple signal based system for switching stack views

--- src/viewer/ui/mainwindow.py
+++ src/viewer/ui/mainwindow.py
@@ -9,15 +9,12 @@ template = os.path.dirname(__file__) + "/mainwindow.ui"
 
 @Gtk.Template(filename=template)
 class Mainwindow(Gtk.ApplicationWindow):
-
     __gtype_name__ = "main_window"
 
     stack = Gtk.Template.Child("main_stack")
 
     def __init__(self, galleryviewwidget, imageviewerwidget):
         super().__init__()
-
-        self.counter = 0
 
         self.imageviewer = imageviewerwidget
         self.galleryviewer = galleryviewwidget
@@ -26,16 +23,12 @@ class Mainwindow(Gtk.ApplicationWindow):
         self.stack.add_named(galleryviewwidget, "galleryviewer")
 
         self.show_all()
+        self.imageviewer.connect("switch_to_gallery_view", self.switch_to_gallery_view)
         self.connect("destroy", Gtk.main_quit)
+
+    def switch_to_gallery_view(self, obj, foo):
+        self.stack.set_visible_child(self.galleryviewer)
+
 
     def start(self):
         Gtk.main()
-
-
-    @Gtk.Template.Callback()
-    def switch_clicked(self, *args):
-        self.counter = self.counter + 1
-        if self.counter % 2:
-            self.stack.set_visible_child(self.imageviewer)
-        else:
-            self.stack.set_visible_child(self.galleryviewer)
--- src/viewer/ui/widgets/imageviewer.py
+++ src/viewer/ui/widgets/imageviewer.py
@@ -4,12 +4,19 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from gi.repository import GObject
 
 template = os.path.dirname(__file__) + "/imageviewer.ui"
 
 @Gtk.Template(filename=template)
 class ImageViewerWidget(Gtk.Box):
     __gtype_name__ = "imageviewer"
+
+
+    __gsignals__ = {
+        "switch_to_gallery_view": (GObject.SIGNAL_RUN_FIRST, None,
+                      (int,))
+    }
 
     image = Gtk.Template.Child("image")
 
@@ -33,3 +40,7 @@ class ImageViewerWidget(Gtk.Box):
         filename = self.viewer.go_prev().current_image().path_as_bytes()
         self.image.set_from_file(filename)
 
+
+    @Gtk.Template.Callback()
+    def back_button_clicked(self, *args):
+        self.emit("switch_to_gallery_view", 1)
