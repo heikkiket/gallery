a9a82262a3612e36cd361de4b7fc344847996e3a
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-09-15 21:05:35 +0300

    refactor(tests) : add fixture for GalleryToml

--- test/unit/imagegallery/gallery/test_gallery.py
+++ test/unit/imagegallery/gallery/test_gallery.py
@@ -1,16 +1,20 @@
+import pytest
+
 from Imagegallery import GalleryToml
+
+@pytest.fixture
+def gallery(gallery_toml):
+    return GalleryToml(gallery_toml)
 
 def test_gallery():
     gallery = GalleryToml({})
     assert gallery.filenames() == []
     assert not gallery.has_images()
 
-def test_initiating(gallery_toml):
-    gallery = GalleryToml(gallery_toml)
+def test_initiating(gallery):
     gallery.has_images()
 
-def test_list_images(gallery_toml):
-    gallery = GalleryToml(gallery_toml)
+def test_list_images(gallery):
     assert gallery.filenames() == [
         "path/to/image1.jpg",
         "path/to/image2.jpg",
