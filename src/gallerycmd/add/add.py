27ca2226f7811f21c1d88fc712a240030dade0b9
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-09-23 22:27:47 +0300

    feat(gallerycmd) : A small start for add command

--- src/gallerycmd/add/add.py
+++ src/gallerycmd/add/add.py
@@ -1,10 +1,15 @@
 import sys
 
 from gallerycmd.parser import subparsers
+from Imagegallery import Imagegallery
 
 def main(args):
     print("Not implemented yet")
 
+def add_image(filename):
+    gallery = Imagegallery.from_disk()
+    gallery.add(filename)
+    return gallery
 
 parser = subparsers.add_parser('add',
     description="Adds new image into gallery",
---
+++ test/integration/gallerycmd/test_add_cmd.py
@@ -0,0 +1,27 @@
+import pytest
+import os
+
+from test.integration.filesystem_helpers import mkdir, mkfile, mkimg
+
+from gallerycmd.add.add import add_image
+
+@pytest.fixture()
+def environment(tmp_path):
+    gallery_dir = tmp_path / "integration_gallery"
+
+    gallery_dir.mkdir()
+    file = open(gallery_dir / "gallery.toml", "w")
+    file.writelines([
+        '["path/to/image.jpg"]\n',
+        'hash = 123456\n',
+        'title = "My first image"\n',
+        'description = "Image description"\n',
+        "tags = ['foo', 'bar', 'baz']\n"
+    ])
+    file.close()
+    mkimg(gallery_dir / "image 4.jpg")
+    os.chdir(gallery_dir)
+
+def test_add_cmd(environment):
+    imagegallery = add_image("image 4.jpg")
+    assert imagegallery.GalleryToml.has("image 4.jpg")
