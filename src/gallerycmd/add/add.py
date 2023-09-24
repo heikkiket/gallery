730803e7b6ea1309d74a5cff2704ab2f57ac870e
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-09-24 22:17:31 +0300

    feat(gallerycmd) : Add function now saves to disk

--- src/gallerycmd/add/add.py
+++ src/gallerycmd/add/add.py
@@ -1,15 +1,17 @@
 import sys
+from filesystem_operations.gallerysaver import GallerySaveError, save_gallery
 
 from gallerycmd.parser import subparsers
 from Imagegallery import Imagegallery
 
 def main(args):
     try:
-        print(
-            add_image(args.filename)
-        )
+        imagegallery = add_image(args.filename)
+        save_gallery(imagegallery.GalleryToml)
     except(FileNotFoundError):
         print("No such image:", args.filename)
+    except(GallerySaveError):
+        print("Saving gallery.toml file failed for some weird reason. This is probably an internal bug.")
 
 def add_image(filename):
     gallery = Imagegallery.from_disk()
