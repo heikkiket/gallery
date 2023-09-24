8930270dd767299c15341a795cbec5a0046279ef
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-09-24 22:04:13 +0300

    refactor(filesystem_operations) : Return early

--- src/filesystem_operations/gallerysaver.py
+++ src/filesystem_operations/gallerysaver.py
@@ -2,11 +2,11 @@ import tomli_w
 from Imagegallery import GalleryToml
 
 def save_gallery(gallery_object):
-    if isinstance(gallery_object, GalleryToml):
-        file = open("gallery.toml", "wb")
-        tomli_w.dump(gallery_object.gallery_toml, file)
-    else:
+    if not isinstance(gallery_object, GalleryToml):
         raise GallerySaveError("Should be GalleryToml object but was ", type(gallery_object))
+
+    file = open("gallery.toml", "wb")
+    tomli_w.dump(gallery_object.gallery_toml, file)
 
 class GallerySaveError(Exception):
     pass
