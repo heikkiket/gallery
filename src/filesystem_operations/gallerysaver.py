0cd453620f9b65e5adc2c7dbb3113cd27aba7d44
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-09-24 22:14:31 +0300

    refactor(filesystem_operations) : Rename a variable
    Is this too Java-like naming style? Not zen of Python at all

--- src/filesystem_operations/gallerysaver.py
+++ src/filesystem_operations/gallerysaver.py
@@ -1,12 +1,12 @@
 import tomli_w
 from Imagegallery import GalleryToml
 
-def save_gallery(gallery_object):
-    if not isinstance(gallery_object, GalleryToml):
-        raise GallerySaveError("Should be GalleryToml object but was ", type(gallery_object))
+def save_gallery(galleryToml):
+    if not isinstance(galleryToml, GalleryToml):
+        raise GallerySaveError("Should be GalleryToml object but was ", type(galleryToml))
 
     file = open("gallery.toml", "wb")
-    tomli_w.dump(gallery_object.gallery_toml, file)
+    tomli_w.dump(galleryToml.gallery_toml, file)
 
 class GallerySaveError(Exception):
     pass
