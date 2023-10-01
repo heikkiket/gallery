456e0ca06b32e15c357d84aeacec8aa3f645cefd
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-10-01 13:12:58 +0300

    refactor(tests) : Rename few tests for add

--- test/integration/gallerycmd/test_add_cmd.py
+++ test/integration/gallerycmd/test_add_cmd.py
@@ -2,11 +2,11 @@ import pytest
 
 from gallerycmd.add.add import add_image
 
-def test_add_cmd(environment):
+def test_add_cmd_adds_image(environment):
     imagegallery = add_image("image 4.jpg")
     assert imagegallery.LibraryToml.has("image 4.jpg")
 
-def test_add_cmd_with_metadata(environment):
+def test_add_cmd_adds_metadata(environment):
     imagegallery = add_image("image 4.jpg", title="test title", tags=["test"], description="foobar")
     image = imagegallery.LibraryToml.get("image 4.jpg")
     assert image["title"] == "test title"
