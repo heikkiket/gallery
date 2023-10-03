efaffaff2bf92cb56468062d8e1c9ef979ae76d7
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-10-03 13:51:01 +0300

    refactor(tests) : gallery_toml -> library_toml

--- test/unit/imagegallery/library_toml/test_library_toml.py
+++ test/unit/imagegallery/library_toml/test_library_toml.py
@@ -3,49 +3,49 @@ import pytest
 from Imagegallery import LibraryToml, ImageMetadata, NoSuchImageError
 
 @pytest.fixture
-def gallery_data(library_toml):
+def library_data(library_toml):
     return LibraryToml(library_toml)
 
 def test_gallery():
-    gallery_data = LibraryToml({})
-    assert gallery_data.filenames() == []
-    assert not gallery_data.has_images()
+    library_data = LibraryToml({})
+    assert library_data.filenames() == []
+    assert not library_data.has_images()
 
-def test_initiating(gallery_data):
-    gallery_data.has_images()
+def test_initiating(library_data):
+    library_data.has_images()
 
-def test_list_images(gallery_data):
-    assert gallery_data.filenames() == [
+def test_list_images(library_data):
+    assert library_data.filenames() == [
         "path/to/image1.jpg",
         "path/to/image2.jpg",
         "path/to/image3.jpg"
     ]
 
-def test_has_not_image(gallery_data):
-    assert not gallery_data.has("foo.jpg")
+def test_has_not_image(library_data):
+    assert not library_data.has("foo.jpg")
 
-def test_has_image(gallery_data):
-    assert gallery_data.has("path/to/image1.jpg")
+def test_has_image(library_data):
+    assert library_data.has("path/to/image1.jpg")
 
-def test_get_throws_when_no_image(gallery_data):
+def test_get_throws_when_no_image(library_data):
     with pytest.raises(NoSuchImageError):
-        gallery_data.get("foo.jpg")
+        library_data.get("foo.jpg")
 
-def test_returns_member_dict(gallery_data):
-    assert gallery_data.get("path/to/image1.jpg") == {
+def test_returns_member_dict(library_data):
+    assert library_data.get("path/to/image1.jpg") == {
             "hash": 123456,
             "title": "My first image",
             "description" : "Image description",
             "tags" : ['foo', 'bar', 'baz']
         }
 
-def test_add(gallery_data):
-    gallery_data.add("foo.jpg")
-    assert gallery_data.has("foo.jpg")
-    assert gallery_data.get("foo.jpg") == {
+def test_add(library_data):
+    library_data.add("foo.jpg")
+    assert library_data.has("foo.jpg")
+    assert library_data.get("foo.jpg") == {
         "title": "", "description": "", "tags": []
     }
 
-def test_add_with_metadata(gallery_data):
-    gallery_data.add("foo.jpg", ImageMetadata(title="test title"))
-    assert gallery_data.get("foo.jpg") == {"title": "test title", "description": "", "tags": []}
+def test_add_with_metadata(library_data):
+    library_data.add("foo.jpg", ImageMetadata(title="test title"))
+    assert library_data.get("foo.jpg") == {"title": "test title", "description": "", "tags": []}
