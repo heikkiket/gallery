1045ba36787c981e81e1f414a23e3f7f408a6ffe
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-10-03 22:02:21 +0300

    refactor(librarysaver) : galleryToml -> libraryToml

--- src/filesystem_operations/librarysaver.py
+++ src/filesystem_operations/librarysaver.py
@@ -1,17 +1,17 @@
 import tomli_w
 from Imagegallery import LibraryToml
 
-def save_library(galleryToml):
-    if not isinstance(galleryToml, LibraryToml):
-        raise LibrarySaveError("Should be LibraryToml object but was ", type(galleryToml))
+def save_library(libraryToml):
+    if not isinstance(libraryToml, LibraryToml):
+        raise LibrarySaveError("Should be LibraryToml object but was ", type(libraryToml))
 
     file = open("library.toml", "wb")
     try:
-        tomli_w.dump(galleryToml.library_toml, file)
+        tomli_w.dump(libraryToml.library_toml, file)
     except Exception as error:
         print("Error saving library to library.toml:", error)
         print("Here's what we tried to save:\n")
-        print(galleryToml.library_toml)
+        print(libraryToml.library_toml)
 
 class LibrarySaveError(Exception):
     pass
