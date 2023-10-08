e3a06d6f07c7cdd2a394dd5ea8959ed7a8e2b353
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-10-08 22:48:12 +0300

    change(gallerycmd) : Add plumbing for edit command

--- src/gallerycmd/edit/edit.py
+++ src/gallerycmd/edit/edit.py
@@ -1,12 +1,18 @@
 from Imagegallery import Imagegallery
+from filesystem_operations.librarysaver import save_library
 
 
 def edit_image(filename, title=None, description=None, tags=None):
     gallery = Imagegallery.from_disk()
-    gallery.add(filename)
 
     if title == None and description == None and tags == None:
         raise NeedsNamedArgument
 
+    gallery.edit(filename,
+                 title=title,
+                 description=description,
+                 tags=tags)
+
+    save_library(gallery.LibraryToml)
 class NeedsNamedArgument(Exception):
     pass
--- test/integration/gallerycmd/test_edit_cmd.py
+++ test/integration/gallerycmd/test_edit_cmd.py
@@ -1,4 +1,5 @@
 import pytest
+from filesystem_operations.libraryreader import load_library
 from gallerycmd.edit import edit_image
 from gallerycmd.edit.edit import NeedsNamedArgument
 
@@ -13,4 +14,9 @@ def test_needs_one_named_argument(environment):
     edit_image("path/to/image1.jpg", title="foo")
     edit_image("path/to/image1.jpg", description="foo")
     edit_image("path/to/image1.jpg", tags=["foo"])
+
+def test_edit_succeeds(environment):
+    edit_image("path/to/image1.jpg", title="new title")
+    toml_file = load_library("library.toml")
+    assert toml_file["path/to/image1.jpg"]["title"] == "new title"
 
