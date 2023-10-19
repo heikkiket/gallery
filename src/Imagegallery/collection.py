9a504732effd69c79e6ae7fe86ffc1c3e26d9ec6
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-10-19 13:46:15 +0300

    refactor(Imagegallery) : Add type hints to Collection

--- src/Imagegallery/collection.py
+++ src/Imagegallery/collection.py
@@ -1,17 +1,19 @@
+from Imagegallery.image import Image
+
 class Collection:
 
-    images = None
+    images = []
 
     def __init__(self, name, hash):
         self.name = name
         self.hash = hash
-        self.images = []
+        self.images: list[Image] = []
 
     def is_empty(self):
         return len(self.images) == 0
 
-    def add_images(self, images):
+    def add_images(self, images: list[Image]):
         self.images.extend(images)
 
-    def add_image(self, image):
+    def add_image(self, image: Image):
         self.images.append(image)
