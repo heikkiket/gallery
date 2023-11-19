b136d2765f160f8cd239f7ecce8a67783dcf5c70
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-11-19 23:10:37 +0200

    feat(viewer) : ImageDetails properties can be updated

--- src/viewer/logic/imagedetails.py
+++ src/viewer/logic/imagedetails.py
@@ -1,15 +1,35 @@
 from gi.repository import GObject
 
 class ImageDetails(GObject.Object):
+    _title = ""
+    _description = ""
+    _tags = ""
 
     @GObject.Property(type=str)
     def title(self):
-        return ""
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
 
     @GObject.Property(type=str)
     def description(self):
-        return ""
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        self._description = value
 
     @GObject.Property(type=str)
     def tags(self):
-        return ""
+        return self._tags
+
+    @tags.setter
+    def tags(self, value):
+        self._tags = value
+
+    def set_image_metadata(self, metadata):
+        self.props.title = metadata.title
+        self.props.description = metadata.description
+        self.props.tags = ", ".join(metadata.tags)
--- test/unit/viewer/imagedetail_test.py
+++ test/unit/viewer/imagedetail_test.py
@@ -1,7 +1,22 @@
+from Imagegallery.imagemetadata import ImageMetadata
 from viewer.logic.imagedetails import ImageDetails
 
-def test_image_details():
+def test_image_details_has_needed_properties():
     imagedetails = ImageDetails()
     assert imagedetails.get_property("title") == ""
     assert imagedetails.get_property("description") == ""
     assert imagedetails.get_property("tags") == ""
+
+def test_set_image_metadata_updates_props():
+    imagedetails = ImageDetails()
+    imagedetails.set_image_metadata(ImageMetadata(title="foo",
+                                                  description="bar",
+                                                  tags=["baz"]))
+    assert imagedetails.get_property("title") == "foo"
+    assert imagedetails.get_property("description") == "bar"
+    assert imagedetails.get_property("tags") == "baz"
+
+def test_several_tags_are_comma_separated():
+    imagedetails = ImageDetails()
+    imagedetails.set_image_metadata(ImageMetadata(tags=["foo", "bar"]))
+    assert imagedetails.get_property("tags") == "foo, bar"
