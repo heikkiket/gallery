52a0f2e4a42724752df8d05ec6a63d33b7b0bace
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-11-19 22:19:00 +0200

    refactor(tests) : Change test case naming

--- test/unit/imagegallery/imagefile_test.py
+++ test/unit/imagegallery/imagefile_test.py
@@ -3,16 +3,16 @@ from pathlib import Path
 from Imagegallery import ImageFile
 
 
-def test_image_has_filename():
+def test_imagefile_has_filename():
     image = ImageFile(Path("foo.img"), "jpg")
     assert image.name == "foo.img"
 
-def test_images_have_different_names():
+def test_imagefiles_have_different_names():
     img1 = ImageFile(Path("img1.jpg"), "jpg")
     img2 = ImageFile(Path("img2.jpg"), "jpg")
     assert img1.name == "img1.jpg"
     assert img2.name == "img2.jpg"
 
-def test_image_returns_path_as_bytes():
+def test_imagefile_returns_path_as_bytes():
     img1 = ImageFile(Path("img1.jpg"), "jpg")
     assert img1.path_as_bytes() == "img1.jpg"
