77d2ebae1ea0860493aac05b643578547df03a71
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-11-19 13:17:11 +0200

    feat(viewer) : A image detail display

---
+++ src/viewer/ui/widgets/imagedetails.py
@@ -0,0 +1,17 @@
+import os
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+template = os.path.dirname(__file__) + "/imagedetails.ui"
+
+@Gtk.Template(filename=template)
+class ImageDetails(Gtk.Grid):
+    __gtype_name__ = "imagedetails"
+
+    def __init__(self):
+        super().__init__()
+
+
