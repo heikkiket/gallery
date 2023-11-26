0c31bebd82f741819f2ef1a051ed679c39dee900
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2023-11-26 22:55:23 +0200

    refactor(viewer) : Restructure method ordering

--- src/viewer/logic/collectionviewer.py
+++ src/viewer/logic/collectionviewer.py
@@ -32,23 +32,6 @@ class CollectionViewer(GObject.Object):
     def count(self):
         return len(self.images)
 
-    def current_image(self) -> Image:
-        return self.images[self.current_index]
-
-    def _update_current_image(self):
-        if self.has_images():
-            self._update_current_image_path()
-            self.current_image_details.set_image_metadata(
-                self.current_image().metadata
-            )
-
-    def _update_current_image_path(self):
-        if self.has_images():
-            self.props.current_image_path = self.current_image().file.path_as_bytes()
-        else:
-            self.props.current_image_path = ""
-
-
     def go_next(self):
         if self.current_index < len(self.images) - 1:
             self.current_index = self.current_index + 1
@@ -64,3 +47,19 @@ class CollectionViewer(GObject.Object):
     def load_collection(self, collection: Collection):
         self.empty()
         self.add_images(collection.images)
+
+    def current_image(self) -> Image:
+        return self.images[self.current_index]
+
+    def _update_current_image(self):
+        if self.has_images():
+            self._update_current_image_path()
+            self.current_image_details.set_image_metadata(
+                self.current_image().metadata
+            )
+
+    def _update_current_image_path(self):
+        if self.has_images():
+            self.props.current_image_path = self.current_image().file.path_as_bytes()
+        else:
+            self.props.current_image_path = ""
