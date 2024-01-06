dc924888ff9e160aeebf8085b763333f4c26cbc8
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2024-01-06 14:22:49 +0200

    fix(viewer) : remove unneeded library import

--- src/viewer/ui/app.py
+++ src/viewer/ui/app.py
@@ -1,5 +1,4 @@
 from Imagegallery import Imagegallery
-from Imagegallery.collections import make_collections
 from viewer.logic import GalleryViewer
 from viewer.ui.mainwindow import Mainwindow
 from viewer.ui.widgets.collectiongrid import CollectionGridWidget
